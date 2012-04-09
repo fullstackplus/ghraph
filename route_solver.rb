05c0632db4972bf62e8a57ca9219c5be3ae0ee4c
James Abbott <james@jamesabbottdd.com>
2012-04-09 10:36:07 +0200

    the step before filling out time digifying functions - the lowest_by functions are working

--- route_solver.rb
+++ route_solver.rb
@@ -10,7 +10,7 @@ REFACTORING:
 2. Create object with a block interface
 3. Array, not Hash in process_indices
 4. Test with objects
-5. Latch the paths algorithm to objects
+5. Latch the paths algorithm on to objects
 
 6. Test with price
 7. Create time_digify function, test 
@@ -25,22 +25,45 @@ class RouteSolver
     def initialize
       yield self
     end
+    
+    def duration
+      digify(@arr) - digify(@dep)
+    end
+    
+    #refactor to module time_arithmetic
+    def digify
+    end
   end
   
   class Route 
     attr_reader :flights
+    attr_accessor :dur #for testing only, use the method duration() in production
     
     def initialize(flights)
       @flights = flights
     end
+    
+    def connects?(origin, destination)
+      @flights.first.from.eql?(origin) && 
+      @flights.last.to.eql?(destination)
+    end
+    
+    def price
+      @flights.map {|flt| flt.price.to_i}.reduce {|sum, price| sum += price}
+    end
+    
+    def duration
+      #@flights.map {|flt| flt.duration}.reduce {|sum, duration| sum += duration}
+      5
+    end
   end
-  
+    
   def initialize(file)
   end
   
   def foo(file_path) 
     File.open(file_path) do |f|
-      create_schedules(f, gather_indices(f))
+      schedules(f, gather_indices(f))
     end
   end
   
@@ -62,14 +85,14 @@ class RouteSolver
     indices
   end
   
-  def create_schedules(file, indices)
+  def schedules(file, indices)
     indices.map do |index| 
       digits = index.split('-')
       num = digits[0].to_i
       inc = digits[1].to_i
       schedules = []
       while inc > 0
-        schedules << create_flight(file, num)
+        schedules << flight(file, num)
         num += 1
         inc -= 1
       end
@@ -77,7 +100,7 @@ class RouteSolver
     end
   end
   
-  def create_flight(file, number)
+  def flight(file, number)
     lines =  File.open(file).readlines
     data = lines[number].split(' ')
     flight = Flight.new do |f|
@@ -90,25 +113,50 @@ class RouteSolver
     flight
   end
   
-  def paths(flight, routes, flt=[])
-    flight = [flight] unless flight.respond_to? :length
+  def routes(flight, schedule, flt=[])
+    flight = [flight] if flt.empty?
     flights = flight + flt
-    adjacent = neighbors(flights, routes)
+    adjacent = neighbors(flights, schedule)
     return [Route.new(flights)] if adjacent.empty?
-    routes = routes - adjacent
+    schedule = schedule - adjacent
     adjacent.map do |flt| 
-      paths(flights, routes, [flt])
+      routes(flights, schedule, [flt])
     end.flatten
   end
   
-  def adjacent(flight1, flight2)
+  def adjacent?(flight1, flight2)
     flight1.to == flight2.from
   end
   
   def neighbors(flights, schedule)
     schedule.select do |flt|
-      adjacent(flights.last, flt)
+      adjacent?(flights.last, flt)
     end
+  end
+  
+  def filter(routes, origin, destination)
+    routes.select do |route| 
+      route.connects?(origin, destination) 
+    end
+  end
+  
+  def routes_between(schedule, origin, destination)
+    all_routes = schedule.select do |flt|
+      flt.from.eql? origin
+    end.map do |flt|
+      routes(flt, schedule)
+    end.flatten
+    filter(all_routes, origin, destination)
+  end
+  
+  def lowest_by_price(routes)
+    lowest = routes.map {|route| route.price}.min
+    routes.select {|route| route.price.eql? lowest}
+  end
+  
+  def lowest_by_duration(routes)
+    lowest = routes.map {|route| route.duration}.min
+    routes.select {|route| route.duration.eql? lowest}
   end
   
 end
@@ -131,7 +179,7 @@ describe "RouteSolver" do
 
   describe "test creation of Flight object given file and index" do 
     it "must instantiate an object with proper attributes" do 
-      flight = @solver.create_flight(@file, 3)
+      flight = @solver.flight(@file, 3)
       flight.from.must_equal  'A'
       flight.to.must_equal    'B'
       flight.dep.must_equal   '09:00'
@@ -142,7 +190,7 @@ describe "RouteSolver" do
 
   describe "test creation of schedules from an array of indices" do
     it "must contain the right number of correct Flight objects" do
-      schedules = @solver.create_schedules(@file, ['3-3', '8-7']) 
+      schedules = @solver.schedules(@file, ['3-3', '8-7']) 
       schedules.length.must_equal 2
   
       first_schedule = schedules[0]
@@ -159,7 +207,7 @@ describe "RouteSolver" do
   
   describe "testing the route creation algorithm" do
     before do 
-      schedules = @solver.create_schedules(@file, ['3-3', '8-7']) 
+      schedules = @solver.schedules(@file, ['3-3', '8-7']) 
       @schedule = schedules[1]
       @flight = @schedule[0]     
     end
@@ -196,16 +244,16 @@ describe "RouteSolver" do
     describe "testing route generation" do
       it "must return the route when no neighboring flights exist (base case)" do
          @flight.to = 'Q'
-         route = @solver.paths(@flight, @schedule)
+         route = @solver.routes(@flight, @schedule)
          route.length.must_equal 1
          route[0].flights.must_equal [@flight]
       end
 
       it "must return the list of all possible routes given a flight and a list of scheduled flights (recursive case)" do
-        routes = @solver.paths(@flight, @schedule)
+        routes = @solver.routes(@flight, @schedule)
         routes.length.must_equal 3
         
-        puts "ROUTES: #{routes}"
+        #puts "ROUTES: #{routes}"
         
         first_route = routes[0]
         first_route.flights.length.must_equal 3
@@ -222,6 +270,65 @@ describe "RouteSolver" do
         third_route.flights.first.price.must_equal '50.00'
         third_route.flights.last.price.must_equal '250.00'
       end
+      
+      it "must tell if the route connects two endpoints (more than one route)" do
+        routes = @solver.routes(@flight, @schedule)
+        routes[0].connects?('A', 'Z').must_equal false
+        routes[1].connects?('A', 'Z').must_equal true
+        routes[2].connects?('A', 'Z').must_equal true
+      end
+      
+      it "must tell if the route connects two endpoints (one route)" do
+        routes = @solver.routes(@flight, @schedule)
+        first_route = routes[0]
+        first_route.flights.pop
+        first_route.flights.pop
+        first_route.flights.length.must_equal 1
+        first_route.connects?('A', 'B').must_equal true
+      end
+      
+      it "must filter out those routes that connect two given endpoints" do
+        routes = @solver.routes(@flight, @schedule)
+        filtered = @solver.filter(routes, 'A', 'Z')
+        filtered.length.must_equal 2
+        filtered[0].connects?('A','Z').must_equal true
+        filtered[1].connects?('A','Z').must_equal true
+      end
+      
+      it "must generate all routes between two given endpoints" do
+        routes = @solver.routes_between(@schedule, 'A', 'Z')
+        routes.length.must_equal 6      
+      end
+      
+      it "must calculate price for route" do
+        routes = @solver.routes_between(@schedule, 'A', 'Z')
+        routes[0].price.must_equal 225
+        routes[5].price.must_equal 275  
+      end
+      
+      it "must calculate route(s) with lowest price" do
+        routes = @solver.routes_between(@schedule, 'A', 'Z')
+        cheapest = @solver.lowest_by_price(routes)
+        cheapest.length.must_equal 1
+        cheapest[0].price.must_equal 225  
+      end
+      
+      #TODO
+      it "must calculate duration for route" do
+        routes = @solver.routes_between(@schedule, 'A', 'Z')
+        routes[0].duration.must_equal 5  
+        routes[5].duration.must_equal 4,5   
+      end
+      
+      #TODO
+      it "must calculate route(s) with lowest duration" do
+        routes = @solver.routes_between(@schedule, 'A', 'Z')
+        shortest = @solver.lowest_by_duration(routes)  
+        shortest.length.must_equal 2
+        shortest[0].duration.must_equal 2,5
+        shortest[1].duration.must_equal 2,5
+      end
+      
     end    
   end
 end
@@ -244,4 +351,49 @@ end
      #<RouteSolver::Flight:0x007fafe395aa20 @from="B", @to="Z", @dep="15:00", @arr="16:30", @price="250.00">
     ]>
 ]
+
+[
+  #<RouteSolver::Route:0x007faf4b19a058 @flights=
+    [#<RouteSolver::Flight:0x007faf4b192808 @from="A", @to="B", @dep="08:00", @arr="09:00", @price="50.00">, 1
+     #<RouteSolver::Flight:0x007faf4b19bbb0 @from="B", @to="C", @dep="10:00", @arr="11:00", @price="75.00">, 1
+     #<RouteSolver::Flight:0x007faf4b19a670 @from="C", @to="Z", @dep="16:00", @arr="19:00", @price="100.00"> 3
+    ]>, 
+  #<RouteSolver::Route:0x007faf4b1a3b80 @flights=
+    [#<RouteSolver::Flight:0x007faf4b192808 @from="A", @to="B", @dep="08:00", @arr="09:00", @price="50.00">, 1
+     #<RouteSolver::Flight:0x007faf4b19b408 @from="B", @to="Z", @dep="15:00", @arr="16:30", @price="250.00"> 1,5
+    ]>, 
+  #<RouteSolver::Route:0x007faf4b1a2e60 @flights=
+    [#<RouteSolver::Flight:0x007faf4b19cc40 @from="A", @to="B", @dep="12:00", @arr="13:00", @price="300.00">, 1
+     #<RouteSolver::Flight:0x007faf4b19bbb0 @from="B", @to="C", @dep="10:00", @arr="11:00", @price="75.00">,  1
+     #<RouteSolver::Flight:0x007faf4b19a670 @from="C", @to="Z", @dep="16:00", @arr="19:00", @price="100.00">  3
+    ]>, 
+  #<RouteSolver::Route:0x007faf4b1a2cf8 @flights=
+    [#<RouteSolver::Flight:0x007faf4b19cc40 @from="A", @to="B", @dep="12:00", @arr="13:00", @price="300.00">, 1
+     #<RouteSolver::Flight:0x007faf4b19b408 @from="B", @to="Z", @dep="15:00", @arr="16:30", @price="250.00">  1,5
+    ]>, 
+  #<RouteSolver::Route:0x007faf4b1a24b0 @flights=
+    [#<RouteSolver::Flight:0x007faf4b19c538 @from="A", @to="C", @dep="14:00", @arr="15:30", @price="175.00">, 1,5
+     #<RouteSolver::Flight:0x007faf4b19acb0 @from="C", @to="B", @dep="15:45", @arr="16:45", @price="50.00">,  1 
+     #<RouteSolver::Flight:0x007faf4b19b408 @from="B", @to="Z", @dep="15:00", @arr="16:30", @price="250.00">  1,5
+    ]>, 
+  #<RouteSolver::Route:0x007faf4b1a22a8 @flights=
+    [#<RouteSolver::Flight:0x007faf4b19c538 @from="A", @to="C", @dep="14:00", @arr="15:30", @price="175.00">, 1,5
+     #<RouteSolver::Flight:0x007faf4b19a670 @from="C", @to="Z", @dep="16:00", @arr="19:00", @price="100.00">  3
+    ]>
+]
 =end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
